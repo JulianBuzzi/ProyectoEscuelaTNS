910a415b47b289cd7e80bc5c9f375cda9ed9f283
JulianBuzzi <eljulianbuzzi@gmail.com>
2023-07-12 14:54:38 -0300

    retoques en proximas fechas

--- src/components/Fechas/Fecha.tsx
+++ src/components/Fechas/Fecha.tsx
@@ -7,9 +7,10 @@ export const Fecha: React.FC = (): JSX.Element => {
       {fechas.map((fecha, index) => (
         <div className="fecha-individual" key={index}>
           <figure className='texto-fecha'>
-            <h1>{fecha.fecha} - {fecha.lugar}</h1>
-            <p>Horario: {fecha.horario} HS</p>
-            <p>Precio: ${fecha.precioEntrada}</p>
+            <h1 className='fecha-lugar'>{fecha.fecha} - {fecha.lugar}</h1>
+            <h3 className='horario-show'>Horario: {fecha.horario} HS</h3>
+            <h3 className='precio-ticket'>Precio: ${fecha.precioEntrada}</h3>
+            <button className='boton-tickets'>Tickets</button>
           </figure>
         </div>
       ))}
--- src/components/shop/shopComponents/BotonCarrito.tsx
+++ src/components/shop/shopComponents/BotonCarrito.tsx
@@ -1,8 +1,12 @@
 import '../../../styles/BotonCarrito.css'
 
 export const BotonCarrito: React.FC = (): JSX.Element => {
+  const productoAgregado = (): undefined => {
+    console.log('añadido')
+  }
+
   return (
-    <button className='boton-carrito'>
+    <button className='boton-carrito' onClick={productoAgregado}>
       Añadir
     </button>
   )
