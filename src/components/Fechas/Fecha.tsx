2f24fabae9bcadf82c607cc6c5a1254d62a67b4c
JulianBuzzi <eljulianbuzzi@gmail.com>
2023-07-12 16:44:09 -0300

    cambios en proximas fechas

--- src/components/Fechas/Fecha.tsx
+++ src/components/Fechas/Fecha.tsx
@@ -4,16 +4,20 @@ import '../../styles/Fecha.css'
 export const Fecha: React.FC = (): JSX.Element => {
   return (
     <div className="contenedor-de-fechas">
-      {fechas.map((fecha, index) => (
-        <div className="fecha-individual" key={index}>
-          <figure className='texto-fecha'>
-            <h1 className='fecha-lugar'>{fecha.fecha} - {fecha.lugar}</h1>
-            <h3 className='horario-show'>Horario: {fecha.horario} HS</h3>
-            <h3 className='precio-ticket'>Precio: ${fecha.precioEntrada}</h3>
-            <button className='boton-tickets'>Tickets</button>
-          </figure>
-        </div>
-      ))}
+      {fechas.map((fecha, index) => {
+        return (
+          <div className="fecha-individual" key={index}>
+            <figure className='texto-fecha'>
+              <h1 className='fecha-lugar'>{fecha.fecha} - {fecha.lugar}</h1>
+              <h3 className='horario-show'>Horario: {fecha.horario} HS</h3>
+              <h3 className='precio-ticket'>Precio: ${fecha.precioEntrada}</h3>
+              <button className='boton-tickets'>
+                <a href={fecha.urlAdquirirTickets}>Tickets</a>
+              </button>
+            </figure>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/data/fechas.ts
+++ src/data/fechas.ts
@@ -3,10 +3,11 @@ interface Fechas {
   lugar: string
   horario: number
   precioEntrada: number
+  urlAdquirirTickets: string // Cuando el usuario da click en el botón ticket se lo redirecciona con este url al sitio donde puede adquirir tickets
 }
 
 export const fechas: Fechas[] = [
-  { fecha: '15 de Julio', lugar: 'Teatro Trinidad Guevara', horario: 21, precioEntrada: 2500 },
-  { fecha: '11 de Agosto', lugar: 'La Caverna de Elme', horario: 23, precioEntrada: 3000 },
-  { fecha: '22 de Noviembre', lugar: 'Parador 5', horario: 19, precioEntrada: 1800 }
+  { fecha: '15 de Julio', lugar: 'Teatro Trinidad Guevara', horario: 21, precioEntrada: 2500, urlAdquirirTickets: 'https://www.ticketportal.com.ar/' },
+  { fecha: '11 de Agosto', lugar: 'La Caverna de Elme', horario: 23, precioEntrada: 3000, urlAdquirirTickets: 'https://www.ticketek.com.ar/' },
+  { fecha: '22 de Noviembre', lugar: 'Parador 5', horario: 19, precioEntrada: 1800, urlAdquirirTickets: 'https://www.ticketek.com.ar/' }
 ]
