6909e5caa1b36b9fbfe00fb30a2e3be4853b7003
JulianBuzzi <eljulianbuzzi@gmail.com>
2023-07-13 10:31:50 -0300

    Se agrega un cartel en shop

--- src/components/shop/Shop.tsx
+++ src/components/shop/Shop.tsx
@@ -1,10 +1,15 @@
 import { Header } from './shopComponents/Header'
 import { ListaDeProductos } from './shopComponents/ListaDeProductos'
+import { useState } from 'react'
 
 export const Shop: React.FC = (): JSX.Element => {
+  const [todosLosProductos, setTodosLosProductos] = useState([])
+  const [total, setTotal] = useState(0)
+  const [contadorProductos, setContadorProductos] = useState(0)
+
   return (
     <>
-      <Header />
+      <Header total={10} contadorProductos={contadorProductos} />
       <ListaDeProductos />
     </>
   )
--- src/components/shop/shopComponents/Header.tsx
+++ src/components/shop/shopComponents/Header.tsx
@@ -1,5 +1,33 @@
-export const Header: React.FC = (): JSX.Element => {
+import { FaShoppingCart } from 'react-icons/fa'
+import { useState } from 'react'
+import '../../../styles/Header.css'
+
+interface Props {
+  total: number
+  contadorProductos: number
+}
+
+export const Header: React.FC<Props> = ({ total, contadorProductos }: Props): JSX.Element => {
+  const [activo, setActivo] = useState(false)
+
   return (
-    <h1>Header</h1>
+    <div className='contenedor-principal-header'>
+      <div className='contenedor-carrito-compras'>
+        <FaShoppingCart className='icono-carrito' onClick={() => { setActivo(!activo) }} />
+      </div>
+      <div className='contenedor-principal-cartel-carrito'>
+      {activo
+        ? (<div className='contenedor-cartel-carrito'>
+            <header className='cartel-carrito'>
+              <p className='texto-cantidad-carrito'>Usted tiene {contadorProductos} productos en carrito</p>
+            </header>
+          </div>)
+        : (<div>
+            <p className='texto-cantidad-carrito'>Carrito Vacio</p>
+          </div>)
+      }
+      <p className='texto-total-carrito'>Total: ${total}</p>
+      </div>
+    </div>
   )
 }
